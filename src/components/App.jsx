8823b60f64310350f4208e3f41a2d97864f0453a
Oleksandr Palamarchuk <palamarchuk.oa93@gmail.com>
2022-12-15 12:15:42 +0200

    add components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+import '../styles/main.css';
+
+import Navbar from '../components/Navbar/Navbar';
+
+import Footer from './Footer/Footer';
+// import Home from 'pages/Home';
+import Projects from 'pages/Projects';
+// import Contacts from 'pages/Contacts';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="App">
+      <Navbar />
+      {/* <Home /> */}
+      <Projects />
+      {/* <Contacts /> */}
+      <Footer />
     </div>
   );
 };
