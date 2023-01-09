b7200374d846a05839c72a43961a3157f46953f9
Oleksandr Palamarchuk <96308880+Oleksandr14@users.noreply.github.com>
2023-01-09 22:41:56 +0200

    changed mail and tel

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -12,13 +12,13 @@ const Contacts = () => {
           <li className="content-list__item">
             <h2 className="title-2">Telegram / WhatsApp</h2>
             <p>
-              <a href="tel:+79051234567">+380 63 389 46 34</a>
+              <a href="tel:+380633894634">+380 63 389 46 34</a>
             </p>
           </li>
           <li className="content-list__item">
             <h2 className="title-2">Email</h2>
             <p>
-              <a href="mailto:webdev@protonmail.com">
+              <a href="mailto:palamarchuk.oa93@gmail.com">
                 palamarchuk.oa93@gmail.com
               </a>
             </p>
